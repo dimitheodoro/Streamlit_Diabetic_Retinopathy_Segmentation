2edba8c4a8684964a27979ea2f86eb9e537295d6
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-01-17 21:56:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import streamlit as st
 import cv2
 from skimage.io import imread
 
-
-# os.chdir(r'C:\Users\User\Desktop\FINAL TUNING\Stremlit_DR')
 FORTH = imread('FORTH.png')
 st.header("Segmentation of Diabetic Retinopathy lesions")
 st.image(FORTH)
@@ -17,9 +15,7 @@ Exudates=r'Test images\Exudates.png'
 Hemmorhages=r'Test images\Hemmorhages.png'
 Microaeurysms=r'Test images\Microaneurysms.png'
 Soft_Exudates=r'Test images\Soft Exudates.png'
-
 images={'Exudates':Exudates,'Hemmorhages':Hemmorhages,'Microaeurysms':Microaeurysms,'Soft Exudates':Soft_Exudates}
-
 
 def load_model(Original_image,lesion):  
     model = load_model(lesion+'_weights.h5',compile=False)
@@ -33,7 +29,6 @@ def load_model(Original_image,lesion):
     predict=(predict>0.5)*255
     return test_image0,predict
 
-
 radiobox = st.sidebar.radio(
      "Choose the type of lesion to be segmented",
      ('Exudates', 'Hemmorhages', 'Microaeurysms','Soft Exudates'))
@@ -42,16 +37,13 @@ selectbox = st.sidebar.selectbox(
      "If you want a demo form our images choose demo ,or else upload your fundus image",
      ('demo', 'upload my image',))
 
-
 if selectbox == 'demo':
     st.title(images[radiobox][12::][:-4]+'_weights.h5')
     col1,col2 = st.beta_columns(2)
     resized_image = cv2.resize(imread( images[radiobox]),None,fx=0.5,fy=0.5)
     with col1:
         st.image(resized_image,caption='original image')
-        
     with col2:
-        # st.image(resized_image,caption='original image')
         _,prediction = load_model(images[radiobox],images[radiobox][12::][:-4]+'_weights.h5')
         resized_prediction = cv2.resize(prediction,None,fx=0.5,fy=0.5)
         st.image(resized_prediction,caption='segmented image')
@@ -64,7 +56,6 @@ if selectbox == 'upload my image':
     with col1:
         st.image(resized_image,caption='original image')
     with col2:
-        # st.image(resized_image,caption='original image')
         _,prediction = load_model(images[radiobox],images[radiobox][12::][:-4]+'_weights.h5')
         resized_prediction = cv2.resize(prediction,None,fx=0.5,fy=0.5)
         st.image(resized_prediction,caption='segmented image')
