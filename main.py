b45c258733a0c13b9bb52cfdc511938b0af6bcef
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-01-18 11:25:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ if selectbox == 'upload my image':
         with col1:
             st.image(resized_image,caption='original image')
         with col2:
-            _,prediction = load_model_(resized_image,images[radiobox][12::][:-4]+'_weights.h5')
+            _,prediction = load_model_(uploaded_file,images[radiobox][12::][:-4]+'_weights.h5')
             new_dims = (resized_image.shape[0],resized_image.shape[1])
             resized_prediction = cv2.resize(prediction.astype('float32'),new_dims)
             st.image(resized_prediction,clamp=True,caption='segmented image')
